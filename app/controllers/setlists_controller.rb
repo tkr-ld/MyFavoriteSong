650013743a81b9372bfdeed96d52421bb21fdf45
takuro.sonobe <tkr.ld.not.nyd@gmail.com>
2018-11-24 04:39:16 +0000

    setlist registration

---
+++ app/controllers/setlists_controller.rb
@@ -0,0 +1,40 @@
+class SetlistsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @musician = Musician.find(params[:musician_id])
+    @setlist = @musician.setlists.build
+  end
+
+  def create
+    @setlist = current_user.setlists.build(setlist_params)
+
+    if @setlist.save
+      flash[:success] = 'セットリストを登録しました。'
+      redirect_to @setlist
+    else
+      flash.now[:danger] = 'セットリストの登録に失敗しました。'
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  
+  private
+
+  def setlist_params
+    params.require(:setlist).permit(:title, :date, :place, :musician_id)
+  end
+  
+end
