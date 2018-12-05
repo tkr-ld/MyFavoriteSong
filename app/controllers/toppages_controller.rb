c1e8669fcbb4aa6f18f15dd6320ebb204c1cde87
takuro.sonobe <tkr.ld.not.nyd@gmail.com>
2018-12-05 12:53:48 +0000

    add top setlist

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -3,6 +3,7 @@ class ToppagesController < ApplicationController
     if logged_in?
       redirect_to user_path(current_user) and return
     else
+      @setlists = Setlist.last(5)
       render :layout => nil and return
     end
   end
