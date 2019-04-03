0624235514838b456a6fc892bf69f22bf77cd70d
tkr_ld <tkr.ld.not.nyd@gmail.com>
2019-04-03 18:45:00 +0900

    トップページ修正

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
     end
 
     resources :setlists,except: [:index] do
-      get 'edit_detail', action: :edit_detail
+      get 'edit/track', action: :edit_detail
       resources :songs, only: [:create, :destroy]
     end
     
